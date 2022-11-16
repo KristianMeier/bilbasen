7cbc6df25e56ad79e6d99fc6dd86bdf78df53d07
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-11-16 21:02:06 +0100

    Typing Data

--- src/Backend/Data.js
+++ src/Backend/Data.js
@@ -1,6 +1,6 @@
 const Data = {
-  menuData: {
-    searchData: {
+  menu: {
+    search: {
       columOne: [
         {
           labe: 'Kategori',
@@ -58,21 +58,116 @@ const Data = {
     {
       price: '224.900',
       decription: {
-        name: '',
-        modelYear: '',
-        miles: '',
+        name: 'Mazda 3',
+        modelYear: 'modelår 2020',
+        miles: '39.000 km',
       },
     },
     {
-      price: '',
+      price: '174.000 kr.',
       decription: {
-        name: '',
-        modelYear: '',
-        miles: '',
+        name: 'Mercedes Spring 316',
+        modelYear: 'modelår 2017',
+        miles: '116.000 km',
+      },
+    },
+    {
+      price: '574.900 kr.',
+      decription: {
+        name: 'Volvo XC60',
+        modelYear: 'modelår 2019',
+        miles: '51.000 km',
+      },
+    },
+    {
+      price: '22.900 kr.',
+      decription: {
+        name: 'Peaugeot',
+        modelYear: 'modelår 2004',
+        miles: '102.000 km',
+      },
+    },
+    {
+      price: '379.000 kr.',
+      decription: {
+        name: 'Ford Mustang',
+        modelYear: 'modelår 2014',
+        miles: '133.000 km',
+      },
+    },
+    {
+      price: '1.995 kr.',
+      decription: {
+        name: 'Suzuki Liana',
+        modelYear: 'modelår 2009',
+        miles: '185.000 km',
+      },
+    },
+    {
+      price: '114.800 kr.',
+      decription: {
+        name: 'Mercedes Citon 109',
+        modelYear: 'modelår 2019',
+        miles: '45.000 km',
+      },
+    },
+    {
+      price: '9.000 km',
+      decription: {
+        name: 'Mercedes Citan 109',
+        modelYear: 'modelår 2021',
+        miles: '9.000 km',
+      },
+    },
+    {
+      price: '399.900 kr.',
+      decription: {
+        name: 'Ford Kuga',
+        modelYear: 'modelår 2022',
+        miles: '9.000 km',
+      },
+    },
+    {
+      price: '379.800 kr.',
+      decription: {
+        name: 'Ford Kuga',
+        modelYear: 'modelår 2021',
+        miles: '14.000 km',
+      },
+    },
+    {
+      price: '2.585 kr.',
+      decription: {
+        name: 'Ford Transit Custom 370S',
+        modelYear: 'modelår 2017',
+        miles: '90.000 km',
+      },
+    },
+    {
+      price: '99.900 kr.',
+      decription: {
+        name: 'Renault Trafic T27',
+        modelYear: 'modelår 2016',
+        miles: '124.000 km',
       },
     },
   ],
-  footerData: {
+  carsSecondSection: [
+    [
+      ['Audi', 'A1', 'A3', 'A4', 'A5', 'A6'],
+      ['BMW', '318i', '320d', '320i', '520d', '530d'],
+      ['Citroën', 'C1', 'C3', 'C3 Picasso', 'C4', 'C5'],
+      ['Fiat', '500', '500L', 'Panda', 'Punto', 'Qubo'],
+      ['Ford', 'Fiesta', 'Focus', 'Ka', 'Mondeo', 'S-MAX', ''],
+      ['Hyudai', 'i10', 'i20', 'i30', 'i40', 'ix35'],
+      ['Peaugeot', '107', '208', '308', '508', '3008'],
+      ['Skoda', 'Citigo', 'Fabia', 'Octavia', 'Rapid', 'Superb'],
+      ['Toyota', 'Aygo', 'Auris', 'Acensis', 'iW', 'Yaris'],
+      ['VW', 'Golf VII', 'Passat', 'Polo', 'Touran', 'Up!'],
+    ],
+  ],
+  usedCarsAllBrands: ['Alfa Romeo'],
+  footerAndMenu: {
     columnOne: [
       'Sælg bil',
       'Søg din bil',
