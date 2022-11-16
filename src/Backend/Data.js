ce8d44be3c5a115b9446ff2642ff0cc74201483b
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-11-16 20:32:14 +0100

    cleaning project and typing data

---
+++ src/Backend/Data.js
@@ -0,0 +1,113 @@
+const Data = {
+  menuData: {
+    searchData: {
+      columOne: [
+        {
+          labe: 'Kategori',
+          inputText: 'Personbil',
+        },
+        {
+          labe: 'Drivmiddel',
+          inputText: 'Alle',
+        },
+        {
+          labe: 'Pristype',
+          inputText: 'Alle',
+        },
+      ],
+      columTwo: [
+        {
+          labe: 'Mærke',
+          inputText: 'Mærke',
+        },
+        {
+          labe: 'Km',
+          inputText: 'fra',
+        },
+        {
+          labe: 'Km',
+          inputText: 'til',
+        },
+        {
+          labe: 'Pris',
+          inputText: 'fra',
+        },
+        {
+          labe: 'Pris',
+          inputText: 'til',
+        },
+      ],
+      columThree: [
+        {
+          labe: '',
+          inputText: '',
+        },
+        {
+          labe: '',
+          inputText: '',
+        },
+        {
+          labe: '',
+          inputText: '',
+        },
+      ],
+    },
+    checkboxes: ['Leasing', 'Ring for pris', 'Nysynet'],
+  },
+  cars: [
+    {
+      price: '224.900',
+      decription: {
+        name: '',
+        modelYear: '',
+        miles: '',
+      },
+    },
+    {
+      price: '',
+      decription: {
+        name: '',
+        modelYear: '',
+        miles: '',
+      },
+    },
+  ],
+  footerData: {
+    columnOne: [
+      'Sælg bil',
+      'Søg din bil',
+      'Gerantisalg via Solgt.com',
+      'Hvad er min bil værd',
+      'Salgsguide',
+    ],
+    columnTwo: [
+      'Køb bil',
+      'Find en forhandler',
+      'bilgafinansiering',
+      'Nummerpladetjek',
+      'Købsguide',
+    ],
+    columnThree: [
+      'Snka om biler',
+      'Fourm',
+      'Brugeranmeldelser',
+      'Bilbasens blog',
+    ],
+    columnFore: [
+      'Min bilbase',
+      'Mine annoncer',
+      'Favoritter',
+      'eamte søgninger',
+    ],
+    columnFire: [
+      'Om bilsbasen',
+      'Presse',
+      'Brugervilkår',
+      'Persondatapolitik',
+      'Cookieindstillinger',
+      'Bannerannoncering',
+      'Sikkerhedsguide',
+      'Hjælp',
+    ],
+  },
+}
